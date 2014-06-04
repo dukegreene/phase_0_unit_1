d18cc198c90adc613567916f33c0d61c2676d515
dukegreene <dukegreene@gmail.com>
2014-06-04 16:10:19 -0400

    solved week 3 exercise 3, created work pad

--- week_3/3_js_variables_objects/my_solution.js
+++ week_3/3_js_variables_objects/my_solution.js
@@ -3,19 +3,20 @@
 // __________________________________________
 // Write your code below.
 
-
-
-
-
+var secretNumber = 7
+var password = 'just open the door'
+var allowedIn = 2===3
+var members = ['John', 'Duke', 'Kellie', 'Mary']
 
 
 
 // __________________________________________
 // Reflection: Use the reflection guidelines to write a reflection here. 
 // 
-// 
-// 
-// 
+// Ok, this one was really fun. Like a puzzle to solve. Are we always supposed to write tests first? I hope so. After I started throwing some variables around in the browser, it clicked and I just started solving the tests backwards, which was way faster. I din't have to write three different arrays.
+// I struggled with the code of the function at first. I kind of had to just stare at it for a few minutes until it clicked that the function runs through a number count and does a test at each number that throws an error if it fails. The assert parameters were also tricky for me to grasp, but they were well named, which made it easier.
+// This article helped me understand how the tests work. http://www.w3schools.com/js/js_errors.asp
+// I remember a lot of this from Code Academy and my Ruby knowledge helps for context. I'm looking forward to doing more of this.
 // 
 // 
 
