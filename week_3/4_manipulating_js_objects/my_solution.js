41e0f2ae4365a1856cc3ad17137a155770021de1
dukegreene <dukegreene@gmail.com>
2014-06-04 17:10:48 -0400

    solved 3.4

--- week_3/4_manipulating_js_objects/my_solution.js
+++ week_3/4_manipulating_js_objects/my_solution.js
@@ -17,8 +17,28 @@ var terah = {
 // __________________________________________
 // Write your code below.
 
-
-
+// Define adam variable as an Object with no properties
+var adam = {}
+// Give adam a name property with value "Adam"
+adam.name = "Adam"
+// Add adam as a spouse property in terah
+terah.spouse = adam
+// Change terah's weight to 125
+terah.weight = 125
+// Remove terah's eyeColor property
+delete terah.eyeColor
+// Give adam a spouse property, assign terah there
+adam.spouse = terah
+// Add a propertyless object to terah as a children property
+terah.children = {}
+// Add a carson object (with property name "Carson") as a property to the terah children property
+terah.children.carson = {name: "Carson"}
+// Add a carter object (with property name "Carter") as a property to the terah children property
+terah.children.carter = {name: "Carter"}
+// Add a colton object (with property name "Colton") as a property to the terah children property
+terah.children.colton = {name: "Colton"}
+// Add children property to adam with the value of terah children
+adam.children = terah.children
 
 
 
@@ -27,10 +47,11 @@ var terah = {
 // __________________________________________
 // Reflection: Use the reflection guidelines
 // 
-// 
-// 
-// 
-// 
+// I feel really good about this so far. The HTML/CSS was trickier for me. I do better with abstract variables than I do with visual layouts, I think.
+// The easiest part about this was assigning the most basic variables. The hardest was figuring out how to add the children in, I said {name = "Colton"} at first and it took me a while to remember the colon notation at the top of this document.
+// I can see this becoming very useful in real life. You could create a user profile for a website with what we've learned so far.
+// Writing pseudocode eally helped me here. I think without it I would have had a harder time reading the assignment instructions. The writing process is like a pep talk mixed with a game plan, I didn't think I'd like pseudocode this much.
+// I'm now regretting that I chickened out of making progress last week when everyone was on JavaScript still. I can't wait to catch up and start diving into code with others.
 // 
 
 
